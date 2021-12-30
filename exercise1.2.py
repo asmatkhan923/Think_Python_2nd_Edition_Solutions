6326caf68edb9b86fe985c4588d531eab64f2ba4
asmatkhan923 <asmatkhan924@gmail.com>
2021-12-30 23:22:40 +0500

    exercise1.2

---
+++ exercise1.2.py
@@ -0,0 +1,9 @@
+# Q: How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+
+kilometers = 10
+
+kilometers_in_mile = 1.61
+
+total_miles = kilometers * kilometers_in_mile
+
+print(total_miles)
---
+++ exercise1.3.py
@@ -0,0 +1,13 @@
+#Q: 3. If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average
+#       pace (time per mile in minutes and seconds)? What is your average speed in
+#       miles per hour?
+
+
+total_ks = 10
+total_miles = 10/1.61
+total_time_in_secs = 42*60 + 42
+
+pace_per_hour = total_ks/total_time_in_secs*60*60
+
+print(pace_per_hour)
+
